40b791c2f7c7b103949a3d181cff54ac155d2a96
derrickboyer3 <derrickboyer3455@gmail.com>
2024-10-24 11:29:25 -0400

    Moved over my prototype for the first scene and player controller and added toggleable rear view camera.

---
+++ Boost Blitz/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public GameObject player;
+    private Rigidbody playerRigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigidbody = player.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            // increase the players velocity in the direction they are facing
+            playerRigidbody.velocity += player.transform.forward * .25f;
+            
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            // increase the players velocity in the opposite direction they are facing
+            playerRigidbody.velocity -= player.transform.forward * .25f;
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            // rotate the player 15 degrees to the left
+            player.transform.Rotate(0, -1, 0);
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            player.transform.Rotate(0, 1, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            player.transform.position = new Vector3(1, 2, 1);
+            player.transform.rotation = Quaternion.identity;
+        }
+    }
+}
---
+++ Boost Blitz/Assets/Scripts/ToggleRearViewCamera.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleRearViewCamera : MonoBehaviour
+{
+    public Camera rearViewCamera;
+    private bool isEnabled;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isEnabled = false;
+        rearViewCamera.depth = -2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        ToggleCamera();
+    }
+
+    void ToggleCamera()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            if (isEnabled)
+            {
+                isEnabled = false;
+                rearViewCamera.depth = -2;
+            }
+            else
+            {
+                isEnabled = true;
+                rearViewCamera.depth = 1;
+            }
+        }
+    }
+}
