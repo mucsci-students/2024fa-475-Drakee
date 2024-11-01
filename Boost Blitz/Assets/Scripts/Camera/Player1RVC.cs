e08d8b23d747ba298fe64cd441d7b109e61b99ab
derrickboyer3 <derrickboyer3455@gmail.com>
2024-11-01 09:51:24 -0400

    Performance changes.

--- Boost Blitz/Assets/Scripts/Camera/Player1RVC.cs
+++ Boost Blitz/Assets/Scripts/Camera/Player1RVC.cs
@@ -1,30 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player1RVC : MonoBehaviour
 {
     public Camera rearViewCamera;
+    public Camera mainCamera;
     private bool isEnabled;
+    private InputActions inputActions;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
+        // Initialize the input actions
+        inputActions = new InputActions();
         isEnabled = false;
         rearViewCamera.depth = -2;
+        rearViewCamera.enabled = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnEnable()
     {
-        if (Input.GetKeyDown(KeyCode.C))
-        {
-            ToggleCamera();
-        }
+        // Enable the ToggleCamera action
+        inputActions.PlayerControls.Player1ToggleCamera.performed += ToggleCamera;
+        inputActions.PlayerControls.Enable();
     }
 
-    void ToggleCamera()
+    void OnDisable()
     {
+        // Disable the ToggleCamera action
+        inputActions.PlayerControls.Player1ToggleCamera.performed -= ToggleCamera;
+        inputActions.PlayerControls.Disable();
+    }
+
+    private void ToggleCamera(InputAction.CallbackContext context)
+    {
+        // Toggle the camera
         isEnabled = !isEnabled;
         rearViewCamera.depth = isEnabled ? 1 : -2;
+        if (rearViewCamera.depth == -2)
+        {
+            // turn off the camera
+            rearViewCamera.enabled = false;
+            mainCamera.enabled = true;
+        } 
+        else
+        {
+            rearViewCamera.enabled = true;
+            mainCamera.enabled = false;
+        }
     }
 }
--- Boost Blitz/Assets/Scripts/Camera/Player2RVC.cs
+++ Boost Blitz/Assets/Scripts/Camera/Player2RVC.cs
@@ -1,30 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player2RVC : MonoBehaviour
 {
     public Camera rearViewCamera;
+    public Camera mainCamera;
     private bool isEnabled;
+    private InputActions inputActions;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
+        // Initialize the input actions
+        inputActions = new InputActions();
         isEnabled = false;
         rearViewCamera.depth = -2;
+        rearViewCamera.enabled = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnEnable()
     {
-        if (Input.GetKeyDown(KeyCode.K))
-        {
-            ToggleCamera();
-        }
+        // Enable the ToggleCamera action
+        inputActions.PlayerControls.Player2ToggleCamera.performed += ToggleCamera;
+        inputActions.PlayerControls.Enable();
     }
 
-    void ToggleCamera()
+    void OnDisable()
     {
+        // Disable the ToggleCamera action
+        inputActions.PlayerControls.Player2ToggleCamera.performed -= ToggleCamera;
+        inputActions.PlayerControls.Disable();
+    }
+
+    private void ToggleCamera(InputAction.CallbackContext context)
+    {
+        // Toggle the camera
         isEnabled = !isEnabled;
         rearViewCamera.depth = isEnabled ? 1 : -2;
+        if (rearViewCamera.depth == -2)
+        {
+            // turn off the camera
+            rearViewCamera.enabled = false;
+            mainCamera.enabled = true;
+        } 
+        else
+        {
+            rearViewCamera.enabled = true;
+            mainCamera.enabled = false;
+        }
     }
 }
