2d9060bb7fd70f825ca796589bca63d2a73704e6
derrickboyer3 <derrickboyer3455@gmail.com>
2024-11-07 09:51:21 -0500

    Adjusted for performance.

--- Boost Blitz/Assets/Scripts/Movement/Player1Movement.cs
+++ Boost Blitz/Assets/Scripts/Movement/Player1Movement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Player1Movement : MonoBehaviour
 {
     public GameObject player;
+    public float dynamicFrictionCoefficient = .05f;
     private Rigidbody playerRigidbody;
 
     // Start is called before the first frame update
@@ -13,18 +14,24 @@ public class Player1Movement : MonoBehaviour
         playerRigidbody = player.GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        AdjustForFriction();
         MovePlayer();
+    }
+
+    void AdjustForFriction()
+    {
+        playerRigidbody.velocity -= playerRigidbody.velocity * 0.05f;
     }
 
     void MovePlayer()
     {
+        // Check for input
         if (Input.GetKey(KeyCode.W))
         {
             // increase the players velocity in the direction they are facing
-            playerRigidbody.velocity += player.transform.forward * .25f;
+            playerRigidbody.velocity += player.transform.forward * 1.5f;
             
         }
         if (Input.GetKey(KeyCode.S))
@@ -32,27 +39,27 @@ public class Player1Movement : MonoBehaviour
             // check to see if the player is moving forward
             if (playerRigidbody.velocity.normalized == player.transform.forward)
             {
-                playerRigidbody.velocity -= player.transform.forward * .5f;
+                playerRigidbody.velocity -= player.transform.forward * 1.75f;
             }
             else 
             {
-                playerRigidbody.velocity -= player.transform.forward * .25f;
+                playerRigidbody.velocity -= player.transform.forward * 1.5f;
             }
             
         }
         if (Input.GetKey(KeyCode.A))
         {
             // rotate the player 15 degrees to the left
-            player.transform.Rotate(0, -1, 0);
+            player.transform.Rotate(0, -4, 0);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            player.transform.Rotate(0, 1, 0);
+            player.transform.Rotate(0, 4, 0);
         }
         if (Input.GetKeyDown(KeyCode.R))
         {
-            player.transform.position = new Vector3(1, 2, 32);
-            player.transform.rotation = new Quaternion(0, 180, 0, 0);
+            player.transform.position = new Vector3(1, .2f, -227.3f);
+            player.transform.rotation = new Quaternion(0, 0, 0, 0);
             playerRigidbody.velocity = new Vector3(0, 0, 0);
         }
     }
